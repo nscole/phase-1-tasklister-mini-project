98b772d080ca8bc830ff5056cbd23a024f8a5c47
Naomi Cole <naomisarahcole@gmail.com>
2022-06-30 11:47:15 +1000

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.addEventListener("submit", e => {
+    e.preventDefault();
+    const form = document.getElementById("create-task-form");
+    const tasks = document.getElementById("new-task-description").value;
+    addTasks(tasks);
+    form.reset();
+  })
 });
+
+
+function addTasks(tasks) {
+  const toDoList = document.getElementById("tasks")
+  const li = document.createElement("li");
+  li.innerText = tasks;
+  toDoList.appendChild(li);
+}
